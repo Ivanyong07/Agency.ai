49d6b3bcbfb08750634b9625b747f3a1df1f5836
Ivan Yong Pak Theng <ivanyong07@gmail.com>
2026-05-18 22:50:10 +0800

    Add hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import assets from '../assets/assets'
+
+const Hero = () => {
+  return (
+    <div id='hero' className='flex flex-col items-center
+    gap-6 py-20 px-4 sm:px-12 lg:px-24 xl:px-40 text-center w-full
+    overflow-hidden text-gray-700 dark:text-white'>
+
+        <div>
+            <img className='w-20' src={assets.group_profile} alt="" />
+            <p >Trusted by 10k people</p>
+        </div>
+       
+    </div>
+  )
+}
+
+export default Hero
