64a8b7c27b9d5c9abf4fa42d007cfef07a66e6af
Ivan Yong Pak Theng <ivanyong07@gmail.com>
2026-05-19 20:03:22 +0800

    Add titel and picture

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,9 +7,22 @@ const Hero = () => {
     gap-6 py-20 px-4 sm:px-12 lg:px-24 xl:px-40 text-center w-full
     overflow-hidden text-gray-700 dark:text-white'>
 
-        <div>
+        <div className='inline-flex items-center gap-2 border border-gray-300 p-1.5 pr-4 rounded-full'>
             <img className='w-20' src={assets.group_profile} alt="" />
-            <p >Trusted by 10k people</p>
+            <p className='text-xs font-medium'>Trusted by 10k people</p>
+        </div>
+
+        <h1 className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px]
+        max-w-5xl'>Turning imagination into <span className='bg-gradient-to-r from-blue-700 
+        via-cyan-400 to-purple-500 bg-clip-text text-transparent'>Digital</span> impact.</h1>
+
+        <p className='text-sm sm:text-lg font-medium 
+      text-gray-500 dark:text-white/75 max-w-[80%]
+      sm:max-w-lg pb-3'>Creating meaningful connections and turning big ideas into interactive digital experiences.</p>
+
+        <div className='relative'>
+          <img src={assets.hero_img} alt="" className='w-full max-w-6xl'/>
+          {/* <img src={assets.bgImage2} alt="" className='absolute top-0 right-0 w-40 z-1 w-600'/> */}
         </div>
        
     </div>
