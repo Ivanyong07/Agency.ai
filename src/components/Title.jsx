742e862670f79959b62fb49b0458c59b930ff56d
Ivan Yong Pak Theng <ivanyong07@gmail.com>
2026-05-19 20:45:50 +0800

    Add new file

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Title(title, desc) {
+export default function Title({title, desc}) {
   return (
-    <div>
-      
+    <div className='text-center'>
+      <h2 className='text-3xl sm:text-5xl font-medium'>{title}</h2>
+      <p className='max-w-lgtext-gray-500 dark:text-white/75 mb-6'>{desc}</p>
     </div>
   )
 }
